1ce447d704220288a179be75038a3d7b05c924bc
ageev-aleksey <ageevav97@gmail.com>
2020-04-08 09:07:46 +0300

    Добавлена печать конфигурации автомата при распознавании входной строки

--- src/regex/Matcher.cpp
+++ src/regex/Matcher.cpp
@@ -5,16 +5,13 @@
 #include "regex/Graph.h"
 #include "regex/Matcher.h"
 #include "regex/util.h"
-#include "bprinter/table_printer.h"
 
 Matcher::Matcher(Graph<std::pair<bool, std::set<size_t>>, char> g) : fsm(g) {};
 
 Matcher Matcher::compile(std::string regex, std::ostream &fileNfsm, std::ostream &fileDfsm, std::ostream &fileMinDfm) {
-    bprinter::TablePrinter tp(&std::cout);
     std::string pn = util::convertToRPN(regex);
     std::cout << "regex to rpn: " << pn << std::endl;
     Graph<Empty, char> nfsm = util::graphFromRegex(pn);
-    auto t1 = util::buildConversionTable()
     fileNfsm << util::graphToDOT(nfsm);
     Graph<std::pair<bool, std::set<size_t>>, char> dfsm = util::convertNFSMtoDFSM2(nfsm);
     fileDfsm << util::graphToDOT(dfsm);
@@ -39,9 +36,17 @@ Matcher& Matcher::operator=(Matcher &&m) {
 bool Matcher::match(std::string str) {
     auto state = fsm.begin();
     bool nextState;
-    for(auto &el : str) {
+    for(auto itrS = str.begin(); itrS != str.end(); ++itrS){
+        char &el = *itrS;
         nextState = false;
         const std::list<Graph<std::pair<bool, std::set<size_t>>, char>::Link> &link = state.getLinks();
+        //Печать конфигурации
+        std::cout << "(" << state.getIndex() << "; ";
+        for(auto tail = itrS; tail != str.end(); tail++) {
+            std::cout << *tail;
+        }
+        std::cout << ")" << std::endl;
+
         for(auto itr = link.cbegin(); itr != link.cend(); ++itr){
             if(itr->data == el) {
                 nextState = true;
@@ -52,6 +57,7 @@ bool Matcher::match(std::string str) {
         if(!nextState) {
             return false;
         }
+
 
     }
     //isTerminalState
